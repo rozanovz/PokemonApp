bd1da0c71f7c0bcf5777a5165067eee6e7c89035
rozanovz <rozanovz@gmail.com>
2016-05-27 16:25:04 +0300

    rendering all pockemon sprites

--- app/Components/PokemonInfo.js
+++ app/Components/PokemonInfo.js
@@ -4,10 +4,17 @@ var ReactDOM = require('react-dom');
 var PokemonInfo = React.createClass({
 
 	render: function () {
+		if(this.props.info){
+			var a = this.props.info.sprites;
+			var b = [];
+			for (var i in a) {
+				if (a[i]) b.push(<img src={a[i]} alt="" />)
+			}
+		}
 		return !this.props.info ? 
 			null: 
 				(<div>
-					<img src={this.props.info.sprites.front_default} alt=""/>
+					{b}
 				</div>);
 	}
 });
